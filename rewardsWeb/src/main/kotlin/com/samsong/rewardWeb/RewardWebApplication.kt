ca7420d350392bb99b64d1fb1acd7be8484dde92
Sam Song <sssamsongxin1@gmail.com>
2025-02-27 21:44:38 -0800

    create rewards folder, move code there, add rewardsWeb Sprint boot init

---
+++ rewardsWeb/src/main/kotlin/com/samsong/rewardWeb/RewardWebApplication.kt
@@ -0,0 +1,11 @@
+package com.samsong.rewardWeb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RewardWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<RewardWebApplication>(*args)
+}
