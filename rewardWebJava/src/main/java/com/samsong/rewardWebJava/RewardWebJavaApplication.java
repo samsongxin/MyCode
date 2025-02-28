4aca8c77abe0d8f628549a6fbfab0a9766256513
Sam Song <sssamsongxin1@gmail.com>
2025-02-27 22:30:24 -0800

    add rewardWebJava

---
+++ rewardWebJava/src/main/java/com/samsong/rewardWebJava/RewardWebJavaApplication.java
@@ -0,0 +1,13 @@
+package com.samsong.rewardWebJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardWebJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardWebJavaApplication.class, args);
+	}
+
+}
---
+++ rewardWebJava/src/test/java/com/samsong/rewardWebJava/RewardWebJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsong.rewardWebJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardWebJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
